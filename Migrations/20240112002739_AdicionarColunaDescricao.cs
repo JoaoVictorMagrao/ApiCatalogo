56d04937de401f446ef4345fd333ad21d0018dce
João Victor Magrão <joaovmagrao10@gmail.com>
2024-01-14 10:14:06 -0300

    rotas de POST e GET criada no controller produtos

---
+++ Migrations/20240112002739_AdicionarColunaDescricao.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalago.Migrations
+{
+    public partial class AdicionarColunaDescricao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Preco",
+                table: "Produtos",
+                type: "decimal(10,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Descricao",
+                table: "Produtos");
+
+            migrationBuilder.DropColumn(
+                name: "Preco",
+                table: "Produtos");
+        }
+    }
+}
